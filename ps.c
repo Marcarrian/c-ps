8c8339e252640d0bf2e445a85a494c6e62cc8da2
Marc Muster <marc.muster@bluewin.ch>
2018-12-22 11:06:27 +0100

    isProcessDir function

--- ps.c
+++ ps.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DEBUG 1 // 0 for production
+
+bool isProcessDirectory(const struct dirent *dirent);
 
 int main() {
-  printf("Hello World!");
+  struct dirent *dirent;
 
+  DIR *dir = opendir("/proc");
+
+  if (dir == NULL) {
+    printf("Could not open proc directory");
+    return 1;
+  }
+
+  while ((dirent = readdir(dir)) != NULL) {
+    bool isProcessDir = isProcessDirectory(dirent);
+    if (isProcessDir) {
+      #if DEBUG
+      printf("found a process dir %s\n", dirent->d_name);
+      #endif
+    } else {
+      #if DEBUG
+      printf("not a process dir %s\n", dirent->d_name);
+      #endif
+    }
+  }
+
+  closedir(dir);
   return 0;
+}
+
+/**
+ * Checks if the given dirent is a valid process directory.
+ * Meaning it needs to be a directory and the name needs to be a number;
+ *
+ * Returns true if valid process directory
+ */
+bool isProcessDirectory(const struct dirent *dirent) {
+  return dirent->d_type == DT_DIR && strtol(dirent->d_name, NULL, 0);
 }
