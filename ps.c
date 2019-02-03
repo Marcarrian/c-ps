4b4c8f70e4ed2e8d9564d206e61c74c825306ce2
Marc Muster <marc.muster@bluewin.ch>
2019-02-03 14:39:06 +0100

    Test passed

--- ps.c
+++ ps.c
@@ -4,16 +4,22 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-#define DEBUG 1 // 0 for production
+#include <unistd.h>
 
 bool isProcessDir(const struct dirent *dirent);
 void handleProcessDir(const char *dirName);
 void openStatusFile(const char *fullPath, const char *dirName);
 char *trimWhitespace(char*);
 
+static uid_t UID;
+static char uidBuffer[32];
+
 int main() {
   struct dirent *dirent;
+  UID = getuid();
+
+  // convert UID to string
+  snprintf(uidBuffer, 32, "%d", UID);
 
   DIR *dir = opendir("/proc");
 
@@ -25,14 +31,7 @@ int main() {
   while ((dirent = readdir(dir)) != NULL) {
     bool isProcDir = isProcessDir(dirent);
     if (isProcDir) {
-      #if DEBUG
-      // printf("found a process dir %s\n", dirent->d_name);
-      #endif
       handleProcessDir(dirent->d_name);
-    } else {
-      #if DEBUG
-      // printf("not a process dir %s\n", dirent->d_name);
-      #endif
     }
   }
 
@@ -42,7 +41,7 @@ int main() {
 
 /**
  * Checks if the given dirent is a valid process directory.
- * Meaning it needs to be a directory and the name needs to be a number;
+ * Meaning it needs to be a directory and the name needs to be a number.
  *
  * Returns true if valid process directory
  */
@@ -77,16 +76,41 @@ void openStatusFile(const char *fullPath, const char *dirName) {
 
   char *nameResult;
   char *vmrssResult;
+  char *uidResult;
+  vmrssResult = "0";
+  bool isMyProcess = false;
   
-  while(fgets(line, sizeof line, statusFile) != NULL) {
+  while(fgets(line, sizeof line, statusFile) != NULL) {    
+    char uid[256];
     char name[256];
     char vmrss[256];
-      
+    
+    if (strstr(line, "Uid")) {
+      strcpy(uid, line);
+      uidResult = strtok(uid, ":");
+      uidResult = strtok(NULL, ":");
+      uidResult = trimWhitespace(uidResult);
+
+      for (int i = strcspn(uidResult, "\t "); i < strlen(uidResult); i++) {
+	uidResult[i] = 0;
+      }
+
+      const int uidFromStatusFile = atoi(uidResult);
+
+      if (uidFromStatusFile == UID) {
+	isMyProcess = true;
+      }
+    }
     if (strstr(line, "Name")) {
       strcpy(name, line);
-      nameResult = strtok(name, ":");
-      nameResult = strtok(NULL, ":");
-      nameResult = trimWhitespace(nameResult);
+
+      const int i = strcspn(name, ":") + 1; // +1 to remove ":" aswell
+      memmove(name, name + i, strlen(name) - i);
+      // search for newline and set it to 0
+      for (int j = strcspn(name, "\n"); j < strlen(name); j++) {
+	name[j] = 0;
+      }
+      nameResult = trimWhitespace(name);
     }
     
     if (strstr(line, "VmRSS")) {
@@ -94,28 +118,41 @@ void openStatusFile(const char *fullPath, const char *dirName) {
       vmrssResult = strtok(vmrss, ":");
       vmrssResult = strtok(NULL, ":");
       vmrssResult = trimWhitespace(vmrssResult);
+      // remove "kB" and the trailing whitespace
+      char* kbString;
+      if (NULL != (kbString = strstr(vmrssResult, "kB"))) {
+	kbString[-1] = 0;
+	kbString[0] = 0;
+	kbString[1] = 0;
+      }
     }
+    
   }
-  printf("%s\t%s\t%s\n", dirName, nameResult, vmrssResult);
+  if (isMyProcess) {
+    printf("%5s %16s %6s\n", dirName, nameResult, vmrssResult);
+  }
   fclose(statusFile);
 }
 
 /**
+ * Trims the leading and trailing whitespace of a string
+ *
  * https://stackoverflow.com/a/122721
- * i dont get how end affects the str variale??
  */
 char *trimWhitespace(char *str)
 {
   char *end;
 
   // Trim leading space
+  // Increment the str pointer for each space character
   while(isspace((unsigned char)*str)) str++;
 
   if(*str == 0)  // All spaces?
     return str;
   
   // Trim trailing space
-  end = str + strlen(str) - 1;
+  end = str + strlen(str) - 1; // set the end pointer to the pointer of str + it's length
+  // Decrement the pointer for each space character
   while(end > str && isspace((unsigned char)*end)) end--;
 
   // Write new null terminator character
